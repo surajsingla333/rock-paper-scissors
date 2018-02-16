6b2beb2dbe4cd2a9abdb933f26a1fe46657aa3f0
Suraj Singla <surajsingla333@gmail.com>
2018-02-16 22:32:25 +0530

    Final commit

--- script/main2.js
+++ script/main2.js
@@ -2,102 +2,101 @@ let num = 3, youWin = 0 , youLose = 0 ;
 
 let option ;
 const playerChoice = document.querySelectorAll('.choose');
-playerChoice.forEach( (button) => { 
+playerChoice.forEach( (button) => {
 	 button.addEventListener('click', () => {
-	 
+
 	 option = button.value ;
 	 playRound(option); })
 }) ;
 
 
 
-function random(num) 
-{ 
+function random(num)
+{
 	return Math.floor(Math.random()*num)  ;
-}  
+}
 
-function computerPlay()  
-{	
+function computerPlay()
+{
 	let sh = parseInt(random(num)) ;
 	if(sh == 0) return "rock";
 	else if(sh == 1) return "paper";
-	else if(sh == 2) return "scissors";  
+	else if(sh == 2) return "scissors";
 }
 
-function playRound(playerSelection , computerSelection) 
+function playRound(playerSelection , computerSelection)
 {
 
-	let res = document.querySelector('h4');
+	let res = document.querySelector('h3');
+			res.textContent = null;
 
 	let image = document.querySelector('#showEmoji');
 
-	computerSelection = computerPlay();	
+	computerSelection = computerPlay();
 	let result ;
-	let head = document.querySelector('h3');
-	    head.textContent = null;
+	// let head = document.querySelector('h3');
+	//     head.textContent = null;
 
-   	if((playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") || (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") || (playerSelection.toLowerCase() == "paper" && computerSelection == "rock")) 
-	{ 
-		result = " You WIN.. " + playerSelection + " beats " + computerSelection + " .. " ; 
-		youWin = ++youWin ; 
-		
+   	if((playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") || (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") || (playerSelection.toLowerCase() == "paper" && computerSelection == "rock"))
+	{
+		result = " You WIN.. " + playerSelection + " beats " + computerSelection + " .. " ;
+		youWin = ++youWin ;
+
 		image.setAttribute('src' , 'styles/happy.png');
-		res.textContent = "YOU WIN!!!" ;
+		res.textContent = "YOU WIN!" ;
 	}
 
-	else if(playerSelection.toLowerCase() == computerSelection) 
+	else if(playerSelection.toLowerCase() == computerSelection)
 	{
-		result = "It's a DRAW.."  
+		result = "It's a DRAW.."
 		image.setAttribute('src' , 'styles/blank.png');
-		res.textContent = "IT'S A DRAW!!!"
+		res.textContent = "IT'S A DRAW!"
 	}
-	else 
+	else
 	{
-		result = " You LOSE.. " + computerSelection + " beats " + playerSelection + " .." ; 
-		youLose = ++youLose ; 
+		result = " You LOSE.. " + computerSelection + " beats " + playerSelection + " .." ;
+		youLose = ++youLose ;
 		image.setAttribute('src' , 'styles/sad.png');
-		res.textContent = "YOU LOSE!!!"
+		res.textContent = "YOU LOSE!"
 	}
-	
-	
+
+
 	let score = document.querySelector('#uScore');
 	let compScore = document.querySelector('#compScore');
-	score.textContent =  parseInt(youWin) ; 
-	compScore.textContent =  parseInt(youLose) ; 
+	score.textContent =  parseInt(youWin) ;
+	compScore.textContent =  parseInt(youLose) ;
 
-	
+
 	if (youWin >= 5)
 	{ alert("YOU ARE THE UTIMATE WINNER!!!!");
-	  youWin = 0 ; youLose = 0; 	
-	  head.textContent = "NEW GAME";
+	  youWin = 0 ; youLose = 0;
+	  res.textContent = "NEW GAME";
 	  score.textContent = 0
 	  compScore.textContent = 0
 	  image.setAttribute('src' , 'styles/hello.png');
-	  res.textContent = null;
 	}
 	else if (youLose >= 5)
 	{ alert("A.I. IS THE BEST AFTERALL!!!!");
 	  youWin = 0 ; youLose = 0;
-	  head.textContent = "NEW GAME";
+	  res.textContent = "NEW GAME";
 	  score.textContent = 0
 	  compScore.textContent = 0
 	  image.setAttribute('src' , 'styles/hello.png');
-	  res.textContent = null;
 	}
 
-	return result; 
-}	
+	return result;
+}
 
-/*function game()  
+/*function game()
 {
 	youWin = 0 ; youLose = 0;
 	let para = document.querySelector('p')
-	for(i=1 ; i<=5 ; i++) 
-	{  
-		playRound(playerSelection = prompt() , computerPlay()) ;  
+	for(i=1 ; i<=5 ; i++)
+	{
+		playRound(playerSelection = prompt() , computerPlay()) ;
 		para.textContent = para.textContent + " </br> " + " choice " + i + " is " + playerSelection + " ." ;
 	}
 	if (youWin > youLose) return "You\'re the Winner with " + youWin + " wins...";
 	else if (youWin < youLose) return "You\'re the Losser with " + youLose + " losses...";
-	else return "It's a Draw..."		
+	else return "It's a Draw..."
 }*/
